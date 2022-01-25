1d4e8ce1d4a68361597888dba88f60af46b5a2d8
Djelouahi Samir <sam.djelouahi@gmail.com>
2022-01-25 21:16:12 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+import allContacts from './contacts.json'
+import { useState } from 'react'; 
+
+let randContact = "";
 
 function App() {
+  
+  const [contacts, setContacts] = useState(allContacts.slice(0,5));
+
+  const addClick = () => {
+    randContact = allContacts[Math.floor(Math.random() * allContacts.length)]
+    const contactsCopy = [...contacts];
+    contactsCopy.unshift(randContact)
+    
+    setContacts(contactsCopy);
+  };
+
+  const sortNameClick = () => {
+    
+    const contactsCopy = [...contacts];
+
+    contactsCopy.sort((a,b)=>{
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+    })
+    
+    setContacts(contactsCopy);
+  };
+
+  const sortPopularityClick = () => {
+    
+    const contactsCopy = [...contacts];
+    contactsCopy.sort((a,b)=>{
+      return a.popularity -  b.popularity;
+    })
+    
+    setContacts(contactsCopy);
+  };
+
+  // const deleteClick = ({c}) => {
+    
+  //   const contactsCopy = [...contacts];
+  //   contactsCopy.filter(contact => word.length > 6);
+  //   setContacts(contactsCopy);
+   
+  // };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addClick}>Add Random Contact button</button>
+      <button onClick={sortNameClick}>Sort by Name</button>
+      <button onClick={sortPopularityClick}>Sort by Popularity</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+          <th>Actions</th>
+        </tr>
+        {/* <pre>{JSON.stringify(randContacts, null, 2)}</pre> */}
+        {contacts.map((cont) => {
+          return (
+            <tr key={cont.id}>
+              <td><img src={cont.pictureUrl} width={100}/></td>
+              <td>{cont.name}</td>
+              <td>{cont.popularity}</td>
+              <td>{cont.wonOscar && `🏆`}</td>
+              <td>{cont.wonEmmy && `🏆`}</td>
+              {/* <th><button onClick={deleteClick}>delete</button></th> */}
+            </tr>
+          )
+        })}
+        
+      </table>
     </div>
   );
 }
